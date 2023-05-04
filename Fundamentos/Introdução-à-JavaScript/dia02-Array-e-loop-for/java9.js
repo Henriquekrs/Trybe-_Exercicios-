7669dd5fbb0a33514bcbe5ba76d983495c93454b
Henriquekrs <ghrduarte@hotmail.com>
2023-05-04 11:13:45 -0300

    Resolucao dos exercicios

---
+++ Fundamentos/Introdução-à-JavaScript/dia02-Array-e-loop-for/java9.js
@@ -0,0 +1,11 @@
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1){
+    numbers.push(index);
+}
+console.log(numbers);
+
+let divisao = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index] / 2)
+} 

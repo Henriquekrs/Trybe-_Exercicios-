e87cc03d8580106d4a36b9794f5e03b093d85e7e
Henriquekrs <ghrduarte@hotmail.com>
2023-06-22 13:58:53 -0300

    Resolução exercicios do dia 02 Sort e map

---
+++ Fundamentos/Introdução-a-Higher-Order-Functions/dia02-sort-e-map/exercise.js
@@ -0,0 +1,32 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age - b.age);
+
+console.log(people);
+console.log("=========");
+
+const people2 = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people2.sort((a, b) => b.age - a.age);
+
+console.log(people2);
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const newList = (product, price) => product.map((products, index) => ({ [products]: price[index]}));
+
+const listUpdate = newList(products, prices);
+console.log(listUpdate);
